72cb3b7ff22fa03be2da7c611dc51a185323cf32
Paulo Orestes Formigoni <paulo.formigoni@docente.fieb.edu.br>
2022-11-23 09:12:40 -0300

    Localiza letras e palavras e conta

---
+++ localizador-de-palavras.py
@@ -0,0 +1,122 @@
+# Program developed to locate words in a text, in addition to counting the number of letters by type
+# Developer Prof. Me. Paulo Orestes Formigoni, PhD
+# profpauloformigoni@gmail.com
+# November 2022
+#Lyrics of the song "Brazil, my Brazilian Brazil"
+str1 = """
+Brasil, meu Brasil brasileiro
+Mulato inzoneiro
+Vou cantar-te nos meus versos
+Brasil, samba que dá
+Bamboleio que faz gingar
+O Brasil do meu amor
+Terra de nosso nenhor
+Abre a cortina do passado
+Tira a mãe preta do cerrado
+Bota o rei congo no congado
+Canta de novo o trovador
+A merencória luz da lua
+Toda a canção do seu amor
+Quero ver essa dona caminhando
+Pelos salões arrastando
+O seu vestido rendado
+Esse coqueiro que dá coco
+Oi, onde eu amarro a minha rede
+Nas noites claras de luar
+Oi, essas fontes murmurantes
+Onde eu mato a minha sede
+Onde a lua vem brincar
+Esse Brasil lindo e trigueiro
+É o meu Brasil brasileiro
+Terra de samba e pandeiro
+Brasil, terra boa e gostosa
+Da morena sestrosa
+De olhar indiferente
+Brasil, samba que dá
+Para o mundo se admirar
+O Brasil do meu amor
+Terra de nosso senhor
+Abre a cortina do passado
+Tira a mãe preta do cerrado
+Bota o rei congo no congado
+Canta de novo o trovador
+A merencória luz da lua
+Toda a canção do seu amor
+Essa dona caminhando
+Pelos salões arrastando
+O seu vestido rendado
+Esse coqueiro que dá coco
+Onde amarro a minha rede
+Nas noites claras de luar
+Essas fontes murmurantes
+Onde eu mato a minha sede
+Onde a lua vem brincar
+Esse Brasil lindo e trigueiro
+É o meu Brasil brasileiro
+Terra de samba e pandeiro
+Brasil, meu Brasil brasileiro
+Mulato inzoneiro
+Vou cantar-te nos meus versos
+Brasil, samba que dá
+Bamboleio que faz gingar
+O Brasil do meu amor
+Terra de nosso nenhor
+Abre a cortina do passado
+Tira a mãe preta do cerrado
+Bota o rei congo no congado
+Canta de novo o trovador
+A merencória luz da lua
+Toda a canção do seu amor
+Quero ver essa dona caminhando
+Pelos salões arrastando
+O seu vestido rendado
+Esse coqueiro que dá coco
+Onde eu amarro a minha rede
+Nas noites claras de luar
+Oi, essas fontes murmurantes
+Onde eu mato a minha sede
+Onde a lua vem brincar
+Esse Brasil lindo e trigueiro
+Meu Brasil brasileiro
+Terra de samba e pandeiro
+Oi, essas fontes murmurantes
+Onde eu mato a minha sede
+Onde a lua vem brincar
+Esse Brasil lindo e trigueiro
+É o meu Brasil brasileiro
+Terra de samba e pandeiro
+Brasil"""
+
+
+
+a=print('a or A = ', (str1.count("a") + str1.count("A"))) 
+b=print('b or B = ', (str1.count("b") + str1.count("B"))) 
+c=print('c or C = ', (str1.count("c") + str1.count("C"))) 
+d=print('d or D = ', (str1.count("d") + str1.count("D"))) 
+e=print('e or E = ', (str1.count("e") + str1.count("E"))) 
+f=print('f or F = ', (str1.count("f") + str1.count("F"))) 
+g=print('g or G = ', (str1.count("g") + str1.count("G")))
+h=print('h or H = ', (str1.count("h") + str1.count("H"))) 
+i=print('i or I = ', (str1.count("i") + str1.count("I")))  
+j=print('j or J = ', (str1.count("j") + str1.count("J"))) 
+k=print('k or K = ', (str1.count("k") + str1.count("K"))) 
+l=print('l or L = ', (str1.count("l") + str1.count("L"))) 
+m=print('m or M = ', (str1.count("m") + str1.count("M"))) 
+n=print('n or N = ', (str1.count("n") + str1.count("N"))) 
+o=print('o or O = ', (str1.count("o") + str1.count("O"))) 
+p=print('p or P = ', (str1.count("p") + str1.count("P")))
+q=print('q or Q = ', (str1.count("q") + str1.count("Q"))) 
+r=print('r or R = ', (str1.count("r") + str1.count("R")))  
+s=print('s or S = ', (str1.count("s") + str1.count("S"))) 
+t=print('t or T = ', (str1.count("t") + str1.count("T"))) 
+u=print('u or U = ', (str1.count("u") + str1.count("U"))) 
+v=print('v or V = ', (str1.count("v") + str1.count("V")))
+w=print('w or W = ', (str1.count("w") + str1.count("W"))) 
+x=print('x or X = ', (str1.count("x") + str1.count("X"))) 
+y=print('y or y = ', (str1.count("y") + str1.count("Y"))) 
+z=print('z or Z = ', (str1.count("z") + str1.count("Z"))) 
+espaco=print('espaço = ', str1.count(" ")) 
+Brasil=print('Brasil = ', str1.count("Brasil"))
+
+
+
